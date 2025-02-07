7349a015392456fd59e3336af76987d0e865bb53
Nicholas Jones <nicholassjones2003@gmail.com>
2025-02-07 11:43:58 -0600

    Changed Manager Spawner
    Manager spawner now destroys itself after spawning anything it needs. It also spawns sound manager now just in case.

--- Assets/Scripts/DevToolScripts/ManagersManager.cs
+++ Assets/Scripts/DevToolScripts/ManagersManager.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+
+// // // THE PURPOSE OF THIS SCRIPT IS TO ENSURE THAT A NEEDED MANAGERS ARE GAURENTEED TO BE IN GAME SCENE // // //
+// // // WILL DESTROY ITSELF AFTER CHECKING // // //
+
+
 public class ManagersManager : MonoBehaviour
 {
     private void Awake()
@@ -9,6 +14,18 @@ public class ManagersManager : MonoBehaviour
         {
             // Dynamically load the SoundManager prefab
             Instantiate(Resources.Load<GameObject>("GameManager"));
+            Debug.Log("GameManager doesn't exist! Spawning it in...");
         }
+
+        // This script ensures that on every scene load that a SoundManager is present
+        if (SoundManager.Instance == null)
+        {
+            // Dynamically load the SoundManager prefab
+            Instantiate(Resources.Load<SoundManager>("SoundManager"));
+            Debug.Log("SoundManager doesn't exist! Spawning it in...");
+        }
+
+        Debug.Log("Destroying Manager Spawner");
+        Destroy(gameObject);
     }
 }
